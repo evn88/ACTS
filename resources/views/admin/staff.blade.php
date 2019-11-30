75f1bb72bfd51b8d47d2d29f3f15e537fe33bd01
Egor <evn88fx64@gmail.com>
2019-11-30 16:10:09 +0400

    reformat navbar (staff page)

--- resources/views/admin/staff.blade.php
+++ resources/views/admin/staff.blade.php
@@ -6,28 +6,65 @@
 <p class="heading">В данном разделе вы можете управлять сотрудниками, которые должны проходить обучение:
     добавлять и удалять их, распределять их по учебным группам, редактировать информацию.</p>
 
-<div class="row_1">
+<nav class="navbar navbar-expand-lg navbar-light bg-light my-3">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <form class="form-inline my-2 my-lg-0 mr-auto">
+      <input class="form-control mr-sm-2" type="search"  placeholder="Введите имя" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
+    </form>
+
+    <ul class="navbar-nav">
+      <li class="nav-item btn">
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#importStaffModal">Импорт из csv</a>
+      </li>
+      <li class="nav-item dropdown btn">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Действия
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Переместить в другую группу</a>
+            <a class="dropdown-item" href="#">Приостановить обучение</a>
+            <a class="dropdown-item" href="#">Назначить обучение</a>
+            <a class="dropdown-item" href="#">Удалить</a>
+        </div>
+      </li>
+      <li class="nav-item btn">
+        <a href="#" class="nav-link" data-toggle="modal" data-target="#addStaffModal">Пригласить сотрудника</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+
+
+<!-- <div class="row_1">
     <form class="form-inline">
         <input class="form-control mr-sm-2" type="search" placeholder="Введите имя" aria-label="Search">
         <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
             <i class="fa fa-search" aria-hidden="true"></i> Поиск</button>
     </form>
+
+
     <div class="wrap-staff">
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Действия
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Переместить в другую группу</a>
-                <a class="dropdown-item" href="#">Приостановить обучение</a>
-                <a class="dropdown-item" href="#">Назначить обучение</a>
-                <a class="dropdown-item" href="#">Удалить</a>
+
+            <a href="#" class="btn btn-secondary" data-toggle="modal" data-target="#addStaffModal">Импорт из csv</a>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Действия
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Переместить в другую группу</a>
+                    <a class="dropdown-item" href="#">Приостановить обучение</a>
+                    <a class="dropdown-item" href="#">Назначить обучение</a>
+                    <a class="dropdown-item" href="#">Удалить</a>
+                </div>
             </div>
-        </div>
-        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#addStaffModal">Добавить сотрудника</a>
+            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#addStaffModal">Пригласить сотрудника</a>
+
     </div>
-</div>
+</div> -->
 
 <table class="table table-striped table-hover">
     <thead style="background: $dblue;">
@@ -131,6 +168,8 @@
                         <label for="inputEmail4">Email</label>
                         <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
                     </div>
+                    <p class="alert alert-info">На этот e-mail будет выслана информация для подтверждения регистрации сотрудника</p>
+
                     <div class="form-group">
                         <label for="inputGroup">Учебная группа</label>
                         <select id="inputGroup" class="form-control">
@@ -150,6 +189,52 @@
 </div>
 
 
+<!-- Modal import -->
+<div class="modal fade" id="importStaffModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Импорт из csv</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Для загрузки данных используйте список сохраненный в формат .csv с запятой в качестве разделителя.
+                Файл должен содержать слежующие поля:</p>
+                <ul>
+                    <li>ФИО</li>
+                    <li>Должность</li>
+                    <li>email</li>
+                </ul>
+                <form>
+                    <div class="input-group mb-3">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="inputGroupFile02">
+                        <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                      </div>
+                    </div>
 
+                    <div class="form-group">
+                        <label for="inputGroup">Учебная группа</label>
+                        <select id="inputGroup" class="form-control">
+                            <option selected>Не назначена</option>
+                            <option>Основная</option>
+                            <option>Потенциальный риск</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                <button type="button" class="btn btn-primary">Сохранить</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
