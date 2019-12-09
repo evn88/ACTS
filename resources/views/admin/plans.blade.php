28fe7ed2cea7245581865358f1b64713b2102cf7
Egor <evn88fx64@gmail.com>
2019-12-09 19:51:41 +0400

    add plans

--- resources/views/admin/plans.blade.php
+++ resources/views/admin/plans.blade.php
@@ -11,43 +11,47 @@
     <div class="card_plan">
         <div class="row_1">
             <div class="cell">
-                <img src="http://acts.local/storage/img/web.png" alt="">
-                <p class="text">Пароли<br>и учетные записи</p>
+                <a href="{{ route('plansinf') }}">
+                    <img src="http://acts.local/storage/img/web.png" alt="">
+                    <p class="text">Пароли<br>и учетные записи</p>
+                </a>
             </div>
             <div class="cell">
-                <img src="http://acts.local/storage/img/mail.png" alt="">
-                <p class="text">Электронная<br>почта</p>
+                <a href="{{ route('plansinf') }}">
+                    <img src="http://acts.local/storage/img/mail.png" alt="">
+                    <p class="text">Электронная<br>почта</p>
+                </a>
             </div>
             <div class="cell">
-                <img src="http://acts.local/storage/img/search.png" alt="">
-                <p class="text">Веб-сайты</p>
+                <a href="{{ route('plansinf') }}">
+                    <img src="http://acts.local/storage/img/search.png" alt="">
+                    <p class="text">Веб-сайты</p>
+                </a>
             </div>
         </div>
         <div class="row_1">
             <div class="cell">
-                <img src="http://acts.local/storage/img/sm.png" alt="">
-                <p class="text">Соц.сети<br>и мессенджеры</p>
+                <a href="{{ route('plansinf') }}">
+                    <img src="http://acts.local/storage/img/sm.png" alt="">
+                    <p class="text">Соц.сети<br>и мессенджеры</p>
+                </a>
             </div>
             <div class="cell">
-                <img src="http://acts.local/storage/img/pc.png" alt="">
-                <p class="text">Безопасность<br>ПК</p>
+                <a href="{{ route('plansinf') }}">
+                    <img src="http://acts.local/storage/img/pc.png" alt="">
+                    <p class="text">Безопасность<br>ПК</p>
+                </a>
             </div>
             <div class="cell">
-                <img src="http://acts.local/storage/img/key.png" alt="">
-                <p class="text">Безопасность<br>мобильных устройств</p>
+                <a href="{{ route('plansinf') }}">
+                    <img src="http://acts.local/storage/img/key.png" alt="">
+                    <p class="text">Безопасность<br>мобильных устройств</p>
+                </a>
             </div>
         </div>
     </div>
 </div>
-<h3>Информация об успеваемости</h3>
 
-
-
-
-<div class="row_1" style="width: 650px; align-items: baseline;">
-    <p style="font-size: 18px;">Сформировать отчет в формате xls и скачать его</p>
-    <a href="#" class="btn btn-primary">Скачать отчет</a>
-</div>
 
 
 
