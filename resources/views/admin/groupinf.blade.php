7f46ffd6f2f3b98514fe45ca848bb7535174a360
Egor <evn88fx64@gmail.com>
2019-12-13 21:14:57 +0400

    change routes

--- resources/views/admin/groupinf.blade.php
+++ resources/views/admin/groupinf.blade.php
@@ -94,19 +94,19 @@
     <div class="card_plan">
         <div class="row_1">
             <div class="cell">
-                <a href="{{ route('plansinf') }}">
+                <a href="{{ route('admin.plansinf') }}">
                     <img src="http://acts.local/storage/img/web.png" alt="">
                     <p class="text">Пароли<br>и учетные записи</p>
                 </a>
             </div>
             <div class="cell">
-                <a href="{{ route('plansinf') }}">
+                <a href="{{ route('admin.plansinf') }}">
                     <img src="http://acts.local/storage/img/mail.png" alt="">
                     <p class="text">Электронная<br>почта</p>
                 </a>
             </div>
             <div class="cell">
-                <a href="{{ route('plansinf') }}">
+                <a href="{{ route('admin.plansinf') }}">
                     <img src="http://acts.local/storage/img/search.png" alt="">
                     <p class="text">Веб-сайты</p>
                 </a>
@@ -114,19 +114,19 @@
         </div>
         <div class="row_1">
             <div class="cell">
-                <a href="{{ route('plansinf') }}">
+                <a href="{{ route('admin.plansinf') }}">
                     <img src="http://acts.local/storage/img/sm.png" alt="">
                     <p class="text">Соц.сети<br>и мессенджеры</p>
                 </a>
             </div>
             <div class="cell">
-                <a href="{{ route('plansinf') }}">
+                <a href="{{ route('admin.plansinf') }}">
                     <img src="http://acts.local/storage/img/pc.png" alt="">
                     <p class="text">Безопасность<br>ПК</p>
                 </a>
             </div>
             <div class="cell">
-                <a href="{{ route('plansinf') }}">
+                <a href="{{ route('admin.plansinf') }}">
                     <img src="http://acts.local/storage/img/key.png" alt="">
                     <p class="text">Безопасность<br>мобильных устройств</p>
                 </a>
