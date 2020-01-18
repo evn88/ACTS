d5f3df734c65431259d0dbb6e3826caec4dfe918
Egor <evn88fx64@gmail.com>
2020-01-18 14:56:02 +0400

    add lesson & plans & change routes

---
+++ resources/views/course/plansinf.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.course')
+@section('content')
+
+<h3 class="headtxt">Пароли и учетные записи</h3>
+
+<div class="card_plan">
+    <div class="row_1">
+        <div class="cellinf">
+            <h5>Содержание модуля:</h5>
+            <ol class="rectangle">
+                <li><a class="mark" href="#">Список уроков</a>
+                    <ul>
+                        <li><a href="{{ route('course.lesson') }}">Для чего нужно беспокоиться о безопасности паролей</a></li>
+                        <li><a href="">Как придумать сложный пароль</li>
+                        <li><a href="">Как хранить пароли</li>
+                        <li><a href="">Можно ли давать свои пароли третьим лицам</li>
+                        <li><a href="">Что делать, если пароль взломали?</li>
+                        <li><a href="">Почему нужны разные пароли для личных и корпоративных аккаунтов?</li>
+                    </ul>
+                </li>
+
+                <li><a class="mark" href="#">Тест</a>
+                    <ul>
+                        <li><a href="">Проверка знаний по теме</a></li>
+                    </ul>
+                </li>
+                <li><a class="mark" href="#">Имитация атаки</a>
+                    <ul>
+                        <li><a href="">Кто-то пытается взломать ваш аккаунт! Необходимо сменить пароль</a></li>
+                    </ul>
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+
+
+@endsection
