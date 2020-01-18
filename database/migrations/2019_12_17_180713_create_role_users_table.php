672d87783c34026c681c6e218149ba5797072145
Ann Vershkova <aniamanson@gmail.com>
2020-01-19 00:55:01 +0400

    fix type rows

--- database/migrations/2019_12_17_180713_create_role_users_table.php
+++ database/migrations/2019_12_17_180713_create_role_users_table.php
@@ -14,13 +14,13 @@ class CreateRoleUsersTable extends Migration
     public function up()
     {
         Schema::create('role_users', function (Blueprint $table) {
-            $table->integer('user_id');
-            $table->integer('role_id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
-            // проверить позже
-            // $table->unique(['user_id','role_id']);
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            // $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->bigInteger('role_id')->unsigned()->index();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->unique(['user_id','role_id']);
             $table->timestamps();
         });
     }
