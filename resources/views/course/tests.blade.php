66291c8c5f1ae60b440597ae51c2511910fc18cc
Egor <evn88fx64@gmail.com>
2020-01-22 14:04:59 +0400

    edit tests

--- resources/views/course/tests.blade.php
+++ resources/views/course/tests.blade.php
@@ -12,48 +12,52 @@
         </p>
         <hr class="my-4">
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="answ1" id="radio1" value="1">
-            <label class="form-check-label" for="radio1">
+            <input class="form-check-input" type="radio" name="answ1" id="radio1_1" value="1">
+            <label class="form-check-label" for="radio1_1">
                 Создать простой  и запомнить
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="answ1" id="radio2" value="2">
-            <label class="form-check-label" for="radio2">
+            <input class="form-check-input" type="radio" name="answ1" id="radio1_2" value="2">
+            <label class="form-check-label" for="radio1_2">
                 Cпрятать бумажку с паролем в бумажник
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="answ1" id="radio3" value="3">
-            <label class="form-check-label" for="radio3">
+            <input class="form-check-input" type="radio" name="answ1" id="radio1_3" value="3">
+            <label class="form-check-label" for="radio1_3">
                 Записать на видном месте
             </label>
         </div>
     </div>
     <div class="jumbotron">
         <p class="test">
-            <b>2.</b> Иван Иванович редко пользуется почтовым ящиком. Он создал сложный пароль, записал его на бумажке
-            и спрятал между страниц одного из томов собрания сочинений Джека Лондона на книжной полке. Когда
-            ему срочно понадобилось войти в почту, он не смог найти бумажку, потому что забыл, в какой именно
-            том ее спрятал. Что следовало делать, чтобы избежать подобной ситуации?
+            <b>2.</b> Алексей взял за основу пароля слова из любимой песни и год ее создания.
+            Какой из вариантов пароля можно использовать?
         </p>
         <hr class="my-4">
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="answ1" id="radio1" value="1">
-            <label class="form-check-label" for="radio1">
-                Создать простой  и запомнить
+            <input class="form-check-input" type="radio" name="answ2" id="radio2_1" value="1">
+            <label class="form-check-label" for="radio2_1">
+                LittleRedRiddingHoodHitTheRoad1974
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="answ1" id="radio2" value="2">
-            <label class="form-check-label" for="radio2">
-                Cпрятать бумажку с паролем в бумажник
+            <input class="form-check-input" type="radio" name="answ2" id="radio2_2" value="2">
+            <label class="form-check-label" for="radio2_2">
+                Little1Red4Ridding7Hood9Hit##
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="answ1" id="radio3" value="3">
-            <label class="form-check-label" for="radio3">
-                Записать на видном месте
+            <input class="form-check-input" type="radio" name="answ2" id="radio2_3" value="3">
+            <label class="form-check-label" for="radio2_3">
+                LRedRiddingHoodHitRoad74
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="answ2" id="radio2_4" value="4">
+            <label class="form-check-label" for="radio2_4">
+                LRRHHTR74
             </label>
         </div>
     </div>
