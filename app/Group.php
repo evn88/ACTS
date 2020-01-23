9422af0a9ef845bcfd16c9b80b06b52c33db1a17
Ann Vershkova <aniamanson@gmail.com>
2020-01-23 23:19:16 +0400

    date format

--- app/Group.php
+++ app/Group.php
@@ -3,12 +3,46 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Group extends Model
 {
     protected $fillable = [
         'name', 'date_start', 'date_emd', 'status'
     ];
+
+    public function setDateStartAttribute($value)
+    {
+        if($value){
+            $this->attributes['date_start'] = Carbon::createFromFormat('d.m.Y H:i', $value)->format('Y-m-d H:i:s');
+        } else {
+            $this->attributes['date_start'] = null;
+        }
+    }
+
+    public function setDateEndAttribute($value)
+    {
+        if($value){
+            $this->attributes['date_end'] = Carbon::createFromFormat('d.m.Y H:i', $value)->format('Y-m-d H:i:s');
+        } else {
+            $this->attributes['date_end'] = null;
+        }
+    }
+
+    public function getDateStartAttribute($value)
+    {
+        if($value){
+            return Carbon::parse($value)->format('d.m.Y H:i');
+        }
+    }
+    public function getDateEndAttribute($value)
+    {
+        if($value){
+            return Carbon::parse($value)->format('d.m.Y H:i');
+        }
+    }
+
+
 
     public function users()
     {
