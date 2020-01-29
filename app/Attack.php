0305b371b37ea687220cc0539132b17173912520
Egor <evn88fx64@gmail.com>
2020-01-29 23:13:45 +0400

    added fillable

--- app/Attack.php
+++ app/Attack.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attack extends Model
 {
+    protected $fillable = [
+        'plan_id', 'name', 'mail_template'
+    ];
+
     public function plans()
     {
         return $this->belongsTo(Plan::class);
--- app/Test.php
+++ app/Test.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
+    protected $fillable = [
+        'plan_id', 'name', 'question', 'answer'
+    ];
+
     public function plans()
     {
         return $this->belongsTo(Plan::class);
