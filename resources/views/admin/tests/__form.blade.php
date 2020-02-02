b3cbcf6f86070456e9827b3fa2f808e2bd866728
Ann Vershkova <aniamanson@gmail.com>
2020-02-02 15:02:15 +0400

    move buttons in vue-component

--- resources/views/admin/tests/__form.blade.php
+++ resources/views/admin/tests/__form.blade.php
@@ -3,12 +3,7 @@
     <admin-add-test-component></admin-add-test-component>
 
 
-    <div class="row">
-        <div class="form-group butt lesson">
-            <a href="{{ URL::previous() }}" class="btn btn-secondary">Назад</a>
-            <button type="submit" class="btn btn-primary float-right">Сохранить</button>
-        </div>
-    </div>
+
 
 
 @push('scripts')
