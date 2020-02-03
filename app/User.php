46e49a44bb2bd57921e9277d087cfc50536aba06
Ann Vershkova <aniamanson@gmail.com>
2020-02-03 21:35:48 +0400

    fix logout

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name', 'email', 'password', 'profession'
     ];
 
-    protected $touches = ['roles','groups'];
+//    protected $touches = ['roles','groups'];
 
     /**
      * The attributes that should be hidden for arrays.
