42b3c9f719fdfb93b77b76a482801043942b7c85
Egor <evn88fx64@gmail.com>
2020-03-01 21:39:43 +0400

    fix phpdoc

--- app/Http/Controllers/Admin/StaffController.php
+++ app/Http/Controllers/Admin/StaffController.php
@@ -7,8 +7,12 @@ use App\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUser;
 use App\Http\Requests\UpdateUser;
+use Exception;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use App\Mail\Test;
 use App\Mail\Attack;
@@ -16,6 +20,7 @@ use Illuminate\Support\Facades\Mail;
 use \Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class StaffController extends Controller
 {
@@ -34,7 +39,7 @@ class StaffController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Factory|View
      */
     public function index()
     {
@@ -44,16 +49,11 @@ class StaffController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Factory|View
      */
     public function create()
     {
         $groups = Group::all();
-
-//        Mail::to('aniamanson@gmail.com')->send(new Attack());
-//       Mail::to('aniamanson@gmail.com')->send(new Test());
-
-//        dd(createToken());
         return view('admin.staff.create', compact('groups'));
     }
 
@@ -61,8 +61,8 @@ class StaffController extends Controller
      * Store a newly created resource in storage.
      * Создаем пользователя с паролем по умолчанию
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreUser $request
+     * @return RedirectResponse
      */
     public function store(StoreUser $request)
     {
@@ -77,6 +77,7 @@ class StaffController extends Controller
 
         $user->roles()->attach(2); //добавляем пользователя с ролью гостя
 
+        //отправляем пользователю ссылку на сброс пароля
         Password::sendResetLink( ['email' => $user->email], function (Message $message) {
             $message->subject($this->getEmailSubject());
         });
@@ -88,8 +89,8 @@ class StaffController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return void
      */
     public function show($id)
     {
@@ -100,7 +101,7 @@ class StaffController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Factory|View
      */
     public function edit($id)
     {
@@ -114,9 +115,9 @@ class StaffController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param UpdateUser $request
+     * @param int $id
+     * @return RedirectResponse
      */
     public function update(UpdateUser $request, $id)
     {
@@ -135,8 +136,9 @@ class StaffController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return RedirectResponse
+     * @throws Exception
      */
     public function destroy($id)
     {
